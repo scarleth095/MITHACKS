ffe07e50f81b89f65074b2ce055811e54291fdd4
WTBU <wtbuweb@gmail.com>
2016-09-18 02:33:51 -0400

    new

--- MITHACKS/app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-     def image_url()
-        return "https://s3.amazonaws.com/#{Rails.application.config.bucket}/#{self.image_id}/image.png" 
-  end
-    
-    
-    
-    
-end
